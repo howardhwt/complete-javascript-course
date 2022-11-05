63ec36451bd3220e9dbd9ce33882a62462ed08ad
Howard Tam <howard.tam.hw@gmail.com>
2022-11-04 23:05:56 -0400

    fetchAPI

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -1,6 +1,78 @@
-'use strict';
+"use strict";
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
+
+const renderCountry = function (data, className = "") {
+  const html = `
+   <article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}M people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+          </div>
+        </article>`;
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+/// XML Request
+
+// const getCountryandNeighbour = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open("GET", `https://restcountries.com/v2/name/${country}`);
+//   request.send();
+
+//   request.addEventListener("load", function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//     //Render country
+//     renderCountry(data);
+//     //Render country2
+//     const [neighbour] = data.borders;
+//     if (!neighbour) return;
+
+//     //AJAX Call country 2
+//     const request2 = new XMLHttpRequest();
+//     request2.open("GET", `https://restcountries.com/v2/alpha/${neighbour}`);
+//     request2.send();
+
+//     request2.addEventListener("load", function () {
+//       const data2 = JSON.parse(this.responseText);
+//       console.log(data2);
+//       //Render country
+//       renderCountry(data2, "neighbour");
+//     });
+//   });
+// };
+// getCountryandNeighbour("usa");
+
+///////// Fetch API request
+const getCountryData = function (country) {
+  //fetch country 1
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then((res) => res.json())
+    ///the above will also return a promise to so use another "then()" to get to the data
+    .then((data) => {
+      renderCountry(data[0]);
+
+      const neighbour = data[0].borders?.[0];
+      if (!neighbour) return;
+      //fetch country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`)
+        .then((res) => res.json())
+        .then((data) => renderCountry(data, "neighbour"));
+    });
+};
+
+getCountryData("gb");
